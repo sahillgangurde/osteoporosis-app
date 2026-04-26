117fb7f999e35c8594a75d83e112a30877b24ea1
sahillgangurde <sahilgangurde2129@gmail.com>
2026-04-27 02:20:55 +0530

    Configure project for Render deployment

--- app.py
+++ app.py
@@ -255,4 +255,5 @@ def download_report():
     )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
